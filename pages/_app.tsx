3317f1b4334d875f92b205e5a54935e1946f1024
Oluwatobiju Judah <tobijudah@gmail.com>
2021-02-07 17:07:39 +0100

    chore: styled-components setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,25 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { createGlobalStyle, DefaultTheme, GlobalStyleComponent } from "styled-components";
+
+const GlobalStyle: GlobalStyleComponent<{}, DefaultTheme> = createGlobalStyle`
+  body {
+		padding: 0;
+		margin: 0;
+		box-sizing: content-box;
+		background-color: #000;
+		color: #fff;
+		font-size: 18px;
+		font-family: 'Source Sans Pro', sans-serif;
+  }
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+    <>
+    <GlobalStyle />
+    <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
