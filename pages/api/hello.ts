a70a4bddee866eba489fede91f381b0ae23086be
Oluwatobiju Judah <tobijudah@gmail.com>
2021-02-08 00:31:35 +0100

    chore: expand res function argument type

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (_req: any, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { name: string }): void; new(): any } } }) => {
+export default (_req: any, res: {
+  status: (arg0: number) => {
+    (): any;
+    new(): any;
+    json: {
+      (arg0: { name: string }): void;
+      new(): any
+    }
+  } 
+}) => {
   res.status(200).json({ name: 'John Doe' })
 }
