7275d26ec8f95a32d8c2b4d745f5a5560395edf6
Oluwatobiju Judah <tobijudah@gmail.com>
2021-02-14 10:41:26 +0100

    feat: Section Text

--- components/TextDiv.tsx
+++ components/TextDiv.tsx
@@ -13,6 +13,9 @@ export type TextDivContainerProps = {
 const TextDivContainer = styled.div<TextDivContainerProps>`
   height: 100vh;
   width: 40%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   background-color: ${p => p.Color};
 `
 
@@ -23,10 +26,28 @@ const Number = styled.h2`
   margin: 0;
 `
 
+const Text = styled.div`
+  color: #000;
+  font-size: 1.32rem;
+  font-weight: 300;
+  line-height: 2.1rem;
+  padding: 15% 13%;
+  box-sizing: border-box;
+
+  & > p {
+    max-width: 70%;
+    margin: 0;
+  }
+
+`
+
 const TextDiv: React.FC<TextDivProps> = ({ Data }) => {
   return (
     <TextDivContainer Color={Data.color}>
       <Number>{Data.id}</Number>
+      <Text>
+        <p>{Data.text}</p>
+      </Text>
     </TextDivContainer>
   )
 }
