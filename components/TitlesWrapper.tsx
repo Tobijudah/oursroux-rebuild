9e8c56778537be691c9e2a58792509afeab92011
Oluwatobiju Judah <tobijudah@gmail.com>
2021-02-27 15:00:03 +0100

    feat: TitlesWrapper component

---
+++ components/TitlesWrapper.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const TitlesWrapper = styled.section `
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  height: 100%;
+  width: 100%;
+  z-index: 20;
+`
+
+export default TitlesWrapper;
