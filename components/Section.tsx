b4d7b306beeb3d63f51576a5cb24f382e595e36d
Oluwatobiju Judah <tobijudah@gmail.com>
2021-02-28 17:50:54 +0100

    chore: remove Title component from Section component

--- components/Section.tsx
+++ components/Section.tsx
@@ -3,45 +3,45 @@ import Title from "./Title";
 import TextDiv from "./TextDiv";
 import styled from "styled-components";
 import { dataType } from "../pages/api/data";
-import useWindowWidth from '../hooks/useWindowWidth';
+import useWindowWidth from "../hooks/useWindowWidth";
 
 type SectionProps = {
-  Data: dataType;
-}
+	Data: dataType;
+};
 
 type SectionContainerProps = {
-  Data: dataType;
-  WindowWidth: number
-}
+	Data: dataType;
+	WindowWidth: number;
+};
 
 const SectionContainer = styled.section<SectionContainerProps>`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
 	height: 100%;
-  background-image: url(${p => `images/${p.Data.image}.jpg`});
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center center;
-  transform: ${p => `matrix(1, 0, 0, 1, ${
-      p.Data.id === '01' ? p.WindowWidth / 10 :
-      ((parseInt(p.Data.id, 10) - 1) * -6) * (p.WindowWidth / 10)
-    }, 0)`};
-  z-index: ${p => 14-(parseInt(p.Data.id, 10))};
+	background-image: url(${(p) => `images/${p.Data.image}.jpg`});
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-position: center center;
+	transform: ${(p) =>
+		`matrix(1, 0, 0, 1, ${
+			p.Data.id === "01"
+				? p.WindowWidth / 10
+				: (parseInt(p.Data.id, 10) - 1) * -6 * (p.WindowWidth / 10)
+		}, 0)`};
+	z-index: ${(p) => 14 - parseInt(p.Data.id, 10)};
 `;
 
 const Section: React.FC<SectionProps> = ({ Data }) => {
-
-  const size = useWindowWidth();
+	const size = useWindowWidth();
 
 	return (
-    <SectionContainer WindowWidth={size} Data={Data}>
-      <Title>{Data.title}</Title>
-      <TextDiv Data={Data}/>
-    </SectionContainer>
-  );
+		<SectionContainer WindowWidth={size} Data={Data}>
+			<TextDiv Data={Data} />
+		</SectionContainer>
+	);
 };
 
 export default Section;
