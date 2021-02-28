bf62e814291b037f223a3a77aace69b8e1d6d3a2
Oluwatobiju Judah <tobijudah@gmail.com>
2021-02-28 22:28:10 +0100

    feat: scroll direction dependent animation

--- animations/TitleAnimation.ts
+++ animations/TitleAnimation.ts
@@ -4,27 +4,57 @@ import { CustomEase } from "gsap/dist/CustomEase.min.js";
 gsap.registerPlugin(CustomEase);
 
 const TitleAnimation = (
+	direction: "up" | "down",
 	element: gsap.TweenTarget,
 	dataSize: number,
 	translateX: number,
 	current: number,
-	index: number
+	index: number,
+	setState: (value: React.SetStateAction<number>) => void
 ) => {
+	let delayNumber: number;
+	let translateXNumber: number;
+	if (direction === "up") {
+		delayNumber =
+			current + 1 === index ? 0.065 : current > index ? 0.15 : 0;
+		translateXNumber =
+			current > index
+				? translateX
+				: current + 1 === index
+				? -(dataSize * 0.05)
+				: translateX + dataSize * 0.95;
+		setTimeout(() => {
+			setState(translateXNumber);
+		}, 1000);
+	} else if (direction === "down") {
+		delayNumber =
+			current - 1 === index ? 0.025 : index > current ? 0.05 : 0;
+		translateXNumber =
+			current === index
+				? translateX - dataSize * 0.9
+				: index > current
+				? translateX - dataSize * 0.95
+				: current - 1 === index
+				? -(dataSize * 0.05)
+				: translateX;
+		setTimeout(() => {
+			setState(translateXNumber);
+		}, 1000);
+	} else return new Error("wrong direction passed to Title Animation function");
+
 	gsap.to(element, {
-		delay: current + 1 === index ? 0.05 : current > index ? 0.1 : 0,
-		duration: 0.75,
+		duration: 0.85,
+		delay: delayNumber,
+		transform: `matrix(1, 0, 0, 1, ${translateXNumber}, 0)`,
 		ease: CustomEase.create(
 			"custom",
-			"M0,0 C0.482,0 0.528,0.058 0.634,0.194 0.734,0.322 0.61,0.952 1,1 "
+			"M0,0 C0.482,0 0.447,0.143 0.49,0.252 0.556,0.42 0.458,1 1,1 "
 		),
-		transform: `matrix(1, 0, 0, 1, ${
-			current === index || current > index
-				? dataSize * 0.9
-				: current + 1 === index
-				? -72
-				: translateX + dataSize
-		}, 0)`,
 	});
 };
+
+// ALTERNATE EASES
+// "M0,0 C0.482,0 0.528,0.058 0.634,0.194 0.734,0.322 0.61,0.952 1,1 "
+// "M0,0,C0.482,0,0.414,0.349,0.438,0.52,0.462,0.692,0.468,1,1,1"
 
 export default TitleAnimation;
