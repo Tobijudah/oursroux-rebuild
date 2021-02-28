9370a7ad335fb5dad0f1cf2738898d2ec8bfc152
Oluwatobiju Judah <tobijudah@gmail.com>
2021-02-28 17:32:26 +0100

    chore: animation delay and ease

--- animations/TitleAnimation.ts
+++ animations/TitleAnimation.ts
@@ -1,4 +1,7 @@
 import gsap from "gsap";
+import { CustomEase } from "gsap/dist/CustomEase.min.js";
+
+gsap.registerPlugin(CustomEase);
 
 const TitleAnimation = (
 	element: gsap.TweenTarget,
@@ -8,10 +11,14 @@ const TitleAnimation = (
 	index: number
 ) => {
 	gsap.to(element, {
+		delay: current + 1 === index ? 0.05 : current > index ? 0.1 : 0,
 		duration: 0.75,
-		ease: "power3.in",
+		ease: CustomEase.create(
+			"custom",
+			"M0,0 C0.482,0 0.528,0.058 0.634,0.194 0.734,0.322 0.61,0.952 1,1 "
+		),
 		transform: `matrix(1, 0, 0, 1, ${
-			(current === index) || (current > index)
+			current === index || current > index
 				? dataSize * 0.9
 				: current + 1 === index
 				? -72
