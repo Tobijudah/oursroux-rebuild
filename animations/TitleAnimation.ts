cea4384ac5ddf7d698f73be79ed4e29a10053626
Oluwatobiju Judah <tobijudah@gmail.com>
2021-02-28 16:11:59 +0100

    chore: update matrix X logic

--- animations/TitleAnimation.ts
+++ animations/TitleAnimation.ts
@@ -3,12 +3,20 @@ import gsap from "gsap";
 const TitleAnimation = (
 	element: gsap.TweenTarget,
 	dataSize: number,
-	translateX: number
+	translateX: number,
+	current: number,
+	index: number
 ) => {
 	gsap.to(element, {
 		duration: 0.75,
 		ease: "power3.in",
-		transform: `matrix(1, 0, 0, 1, ${translateX + dataSize * 0.9}, 0)`,
+		transform: `matrix(1, 0, 0, 1, ${
+			(current === index) || (current > index)
+				? dataSize * 0.9
+				: current + 1 === index
+				? -72
+				: translateX + dataSize
+		}, 0)`,
 	});
 };
 
