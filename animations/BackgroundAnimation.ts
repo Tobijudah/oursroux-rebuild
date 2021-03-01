d2190e1b65d05dad7b5f088a33782b02abfaf7a5
Oluwatobiju Judah <tobijudah@gmail.com>
2021-03-01 23:36:33 +0100

    feat: BackgroundAnimation

---
+++ animations/BackgroundAnimation.ts
@@ -0,0 +1,54 @@
+import gsap from "gsap";
+import { CustomEase } from "gsap/dist/CustomEase.min.js";
+
+gsap.registerPlugin(CustomEase);
+
+const BackgroundAnimation = (
+	direction: "up" | "down",
+	element: gsap.TweenTarget,
+	dataSize: number,
+	translateX: number,
+	current: number,
+	index: number,
+	setState: (value: React.SetStateAction<number>) => void
+) => {
+	let delayNumber: number;
+	let translateXNumber: number;
+	if (direction === "up") {
+    delayNumber =
+    current + 1 === index || current === index ? 0.035 : 0;
+		translateXNumber =
+			current + 1 === index
+				? dataSize / 10
+				: current === index || current > index
+				? dataSize
+				: translateX + 0.6 * dataSize;
+		setTimeout(() => {
+			setState(translateXNumber);
+		}, 1000);
+	} else if (direction === 'down') {
+    delayNumber =
+    current + 1 === index || current === index ? 0.035 : 0;
+		translateXNumber =
+			current - 1 === index
+				? dataSize / 10
+				: current === index || index > current
+				? translateX - 0.6 * dataSize
+				: dataSize;
+		setTimeout(() => {
+			setState(translateXNumber);
+		}, 1000);
+  } else return new Error("wrong direction passed to Background Animation function");
+
+	gsap.to(element, {
+		duration: 1.2,
+    delay: delayNumber,
+		transform: `matrix(1, 0, 0, 1, ${translateXNumber}, 0)`,
+		ease: CustomEase.create(
+			"custom",
+			"M0,0 C0.482,0 0.447,0.143 0.49,0.252 0.556,0.42 0.458,1 1,1 "
+		),
+	});
+};
+
+export default BackgroundAnimation;
