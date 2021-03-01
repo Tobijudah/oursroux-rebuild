aab58c6deca065929af181f147f7d45f5c2b37f5
Oluwatobiju Judah <tobijudah@gmail.com>
2021-03-01 17:05:28 +0100

    feat: BackgroundWrapper component

---
+++ components/BackgroundsWrapper.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const BackgroundsWrapper =  styled.section`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  width: 100%;
+  height: 100%;
+`
+
+export default BackgroundsWrapper;
