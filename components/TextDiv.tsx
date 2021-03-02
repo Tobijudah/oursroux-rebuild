d84c842a3a1fa297b14a198dfc819b6e2ecc2778
Oluwatobiju Judah <tobijudah@gmail.com>
2021-03-02 16:31:54 +0100

    feat: TextDiv component updates

--- components/TextDiv.tsx
+++ components/TextDiv.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import styled from 'styled-components';
-import { dataType } from '../pages/api/data';
+import React from "react";
+import styled from "styled-components";
 
 export type TextDivProps = {
-  Data: dataType;
-}
+	dataColor: string;
+};
 
-export type TextDivContainerProps = {
-  Color: string;
-}
+const TextDiv = styled.div<TextDivProps>`
+	position: absolute;
+	left: 0;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	height: 100%;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	background-color: ${(p) => p.dataColor};
 
-const TextDivContainer = styled.div<TextDivContainerProps>`
-  height: 100vh;
-  width: 40%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  background-color: ${p => p.Color};
-`
+	&:first-of-type {
+		z-index: 2;
+	  transform: matrix(1, 0, 0, 1, 0, 0);
+	}
+
+	&:not(:first-of-type) {
+		transform: translate(-80%, 0%) matrix(1, 0, 0, 1, 0, 0);
+	}
+`;
 
 const Number = styled.h2`
   color: #000;
@@ -25,7 +34,7 @@ const Number = styled.h2`
   padding: 44.5% 13% 0 13%;
   letter-spacing: -1.5px;
   margin: 0;
-`
+`;
 
 const Text = styled.div`
   font-size: 0.95em;
@@ -38,18 +47,17 @@ const Text = styled.div`
     max-width: 70%;
     margin: 0;
   }
+`;
 
-`
+const e: React.FC<TextDivProps> = ({}) => {
+	return (
+		<>
+			<Number>1</Number>
+			<Text>
+				<p>1</p>
+			</Text>
+		</>
+	);
+};
 
-const TextDiv: React.FC<TextDivProps> = ({ Data }) => {
-  return (
-    <TextDivContainer Color={Data.color}>
-      <Number>{Data.id}</Number>
-      <Text>
-        <p>{Data.text}</p>
-      </Text>
-    </TextDivContainer>
-  )
-}
-
-export default TextDiv
+export default TextDiv;
