bc54bf45e54c6e90e705c1d6288c111746a21d3d
Oluwatobiju Judah <tobijudah@gmail.com>
2021-03-02 20:56:44 +0100

    chore: useRefArray updates

--- hooks/useRefArray.ts
+++ hooks/useRefArray.ts
@@ -4,18 +4,25 @@ export type RefHandle = {
 	startAnimation: (direction: "up" | "down") => void;
 };
 
-const useRefArray = (length: number) => {
+const useRefArray = (length: number, type: "normal" | "handle") => {
 	const [refs, setRefs] = useState([]);
+
+	type === "handle"
+		? (refs as RefObject<RefHandle>[])
+		: (refs as RefObject<HTMLDivElement>[]);
 
 	useEffect(() => {
 		setRefs((refs) =>
 			Array(length)
 				.fill(0)
-				.map((_, i) => refs[i] || createRef<RefHandle>())
+				.map((_, i) =>
+					type === "handle"
+						? refs[i] || createRef<RefHandle>()
+						: refs[i] || createRef<HTMLDivElement>()
+				)
 		);
 	}, [length]);
-
-	return refs as RefObject<RefHandle>[];
+	return refs;
 };
 
 export default useRefArray;
