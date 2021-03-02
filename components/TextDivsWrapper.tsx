b459e6a813f5d59e843bce84b249584a57cff30d
Oluwatobiju Judah <tobijudah@gmail.com>
2021-03-02 16:29:47 +0100

    feat: TextDivsWrapper component

---
+++ components/TextDivsWrapper.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+
+const TextDivsWrapper = styled.section`
+  position: absolute;
+  left: 0;
+  margin: auto;
+  width: 100%;
+  clip: rect(0px, 640px, 800px, 128px);
+  opacity: 1;
+  transform: matrix(1, 0, 0, 1, 0, 0);
+  position: fixed;
+  left: 0;
+  width: 50%;
+  height: 100%;
+  overflow: hidden;
+  z-index: 10;
+ 
+`
+
+export default TextDivsWrapper;
