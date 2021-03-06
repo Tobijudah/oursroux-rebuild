795c56ae1e0aa275a756a28941d053fe7b37dadc
Oluwatobiju Judah <tobijudah@gmail.com>
2021-03-06 21:35:05 +0100

    chore: minor fix

--- components/TextDivsWrapper.tsx
+++ components/TextDivsWrapper.tsx
@@ -5,20 +5,17 @@ type TextDivsWrapperProps = {
 }
 
 const TextDivsWrapper = styled.section<TextDivsWrapperProps>`
-  position: absolute;
-  left: 0;
-  margin: auto;
-  width: 100%;
-  clip: ${p => `rect(0px, ${p.dataSize / 2}px, 100vh, ${p.dataSize / 10}px)`};
-  opacity: 1;
-  transform: matrix(1, 0, 0, 1, 0, 0);
   position: fixed;
+  top: 0;
   left: 0;
   width: 50%;
   height: 100%;
+  margin: auto;
+  clip: ${p => `rect(0px, ${p.dataSize / 2}px, auto, ${p.dataSize / 10}px)`};
+  opacity: 1;
+  transform: matrix(1, 0, 0, 1, 0, 0);
   overflow: hidden;
   z-index: 10;
- 
 `
 
 export default TextDivsWrapper;
